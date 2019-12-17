b4f45dcfa63aa0659b07a308ee1a41ce66e6155c
Jose Albizures <albizures3601@gmail.com>
2019-12-17 13:19:10 +0300

    chore(verbs): remove generated verbs

--- src/verbs/gen/ю/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './юлить.ts';
-export * from './юркнуть.ts';
-export * from './ютиться.ts';
-export * from './юродствовать.ts';
--- src/verbs/gen/ё/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './ёрзать.ts';
-export * from './ёрзать.ts';
-export * from './ёжиться.ts';
-export * from './ёкнуть.ts';
-export * from './ёрничать.ts';
